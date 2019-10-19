9a72df8a0a03e3bb1c6f2f9db90b94c43c75f21a
unknown <samimassah@gmail.com>
2019-10-19 08:22:26 -0700

    translate.smb

---
+++ translate.py
@@ -0,0 +1,23 @@
+from googletrans import Translator
+s = Translator()
+x= 'what are you doing now'
+print('is calculating')
+
+
+res = s.translate( 'hi' , dest = 'fr', src = 'en')
+res = res.text
+
+
+# for displace the result
+res = res.split()
+m = ''
+l = len(res)
+
+for i in range(l ):
+    m += res[l - 1 - i] + ' '
+   
+    
+
+
+print(m)
+
