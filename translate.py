5e32bfb2780872b5a26ecb907ff7a0bdc04cbd48
samcallen11 <56631431+samcallen11@users.noreply.github.com>
2019-10-26 19:53:29 +0330

    این اصلی است ترنزلیتورست

--- translate.py
+++ translate.py
@@ -1,23 +1,14 @@
 from googletrans import Translator
 s = Translator()
-x= 'what are you doing now'
+srclanguage=input("src:")
+destlanguage=input("dest:")
+dest=destlanguage[0]+destlanguage[1]
+dest=dest.lower()
+#to become it unCapslock
+src=srclanguage[0]+srclanguage[1]
+src=src.lower()
+x= input()
 print('is calculating')
-
-
-res = s.translate( x , dest = 'fa', src = 'en')
+res = s.translate( x , dest = dest, src = src)
 res = res.text
-
-
-# for displace the result
-res = res.split()
-m = ''
-l = len(res)
-
-for i in range(l ):
-    m += res[l - 1 - i] + ' '
-   
-    
-
-
-print(m)
-
+print(res)
